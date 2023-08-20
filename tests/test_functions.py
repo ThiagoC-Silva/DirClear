f22badb31cba8737ea03e67426cd5b16ced1ea67
Thiago Carvalho da Silva <thi89248@gmail.com>
2023-08-20 20:05:44 -0300

    Test of folder cleaning

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,17 +1,39 @@
 import os
 from src.functions.file_cleaning_utils import create_directories
 from src.functions.file_cleaning_utils import create_files
+from src.functions.file_cleaning_utils import clean_folders
+from pytest import fixture
 
-def test_create_directories(tmpdir):
+@fixture
+def test_directory_list(tmpdir):
     directory_test = tmpdir.mkdir('directories')
     directory_list = create_directories(directory_test)
-    for directory in directory_list:
+    return directory_list
+
+
+def test_create_directories(test_directory_list):
+    for directory in test_directory_list:
         assert os.path.exists(directory)
 
 
-def test_create_files(tmpdir):
-    directory_test = tmpdir.mkdir('directories')
-    directory_list = create_directories(directory_test)
-    files_list = create_files(directory_list)
+def test_create_files(test_directory_list):
+    files_list = create_files(test_directory_list)
     for file_path in files_list:
         assert os.path.exists(file_path)
+
+
+def test_clean_folders(test_directory_list):
+    full_path_list = []
+    for directory in test_directory_list:
+        dir_with_file = os.path.join(directory, 'file_test.txt')
+        with open(dir_with_file, 'w') as file:
+            pass
+
+    skip_clean_test = [test_directory_list[0], test_directory_list[2]]
+    clean_folders(test_directory_list, skip_clean_test)
+
+    for folder in test_directory_list:
+        if folder not in skip_clean_test:
+            assert len(os.listdir(folder)) == 0
+        else:
+            assert len(os.listdir(folder)) > 0
