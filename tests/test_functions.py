04f991f56a0032ad84ec62ead82da1faebad1d77
Thiago Carvalho da Silva <thi89248@gmail.com>
2023-08-20 18:44:22 -0300

    Files creation test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -7,3 +7,11 @@ def test_create_directories(tmpdir):
     directory_list = create_directories(directory_test)
     for directory in directory_list:
         assert os.path.exists(directory)
+
+
+def test_create_files(tmpdir):
+    directory_test = tmpdir.mkdir('directories')
+    directory_list = create_directories(directory_test)
+    files_list = create_files(directory_list)
+    for file_path in files_list:
+        assert os.path.exists(file_path)
